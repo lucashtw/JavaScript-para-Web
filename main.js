f7d592a2d700c7e2071f83620a157acc02e7fa6d
lucas <lucashtw@gmail.com>
2022-04-27 21:08:48 -0300

    Eventos e lógica

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-function tocaSom(idElementoAudio){
+function tocaSom(seletorAudio){
+    const elemento = document.querySelector(seletorAudio);
+    if (elemento && elemento.localName === 'audio'){
+        elemento.play(); 
+    }
+    else{
+        alert('Elemento não encontrado ou seletor inválido');
+    }
 
-    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -16,10 +22,18 @@ for (let contador = 0; contador < listaDeTeclas.length;contador++) {
     }
 
     //tecla.onclick = tocaSom(idAudio);
-    
-    tecla.onkeydown = function(){
-        tecla.classList.add('ativa');
+    tecla.onkeydown = function(evento){
+        
+        console.log(evento.code == 'Space');
+
+        if(evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa');
+        }
+
     }
+    
+            
+        
 
     tecla.onkeyup = function(){
         tecla.classList.remove('ativa');
